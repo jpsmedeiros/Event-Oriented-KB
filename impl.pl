a9280d7275bf937e9393b90e6f12eff56f2cbb72
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-07-06 21:41:07 -0300

    Definindo nomes das entidades

--- impl.pl
+++ impl.pl
@@ -1,4 +1,16 @@
-entity("Barak Obama").
+% Entities
+% entity(real_name).
+
+entity("Barack Hussein Obama II").
 entity("California").
-entity("Luis XIV").
-entity("Toyota").
+entity("Toyota Motor Corporation").
+
+% Names
+% name(synonymous_name, real_name).
+
+name("Barack", "Barack Hussein Obama II").
+name("Obama", "Barack Hussein Obama II").
+name("Barack Obama", "Barack Hussein Obama II").
+
+name("Cali", "California").
+
